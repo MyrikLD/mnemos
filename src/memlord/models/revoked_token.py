5183a5b17f80e7b6d6978dac81b8b22f6df00734
Myrik <myrik260138@gmail.com>
2026-03-20 23:47:28 +0100

    fix tests

--- src/memlord/models/revoked_token.py
+++ src/memlord/models/revoked_token.py
@@ -7,4 +7,4 @@ class RevokedToken(Base):
     __tablename__ = "revoked_tokens"
 
     jti = sa.Column(sa.String(255), primary_key=True)
-    expires_at = sa.Column(sa.DateTime, nullable=False)
+    expires_at = sa.Column(sa.DateTime, nullable=False, index=True)
